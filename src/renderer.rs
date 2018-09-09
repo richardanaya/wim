9373bb08489ffdf402c30bfc33ae9ac9d3e1cd7b
richard <richard>
2018-09-09 19:29:54 +0000

    fixes

--- src/renderer.rs
+++ src/renderer.rs
@@ -5,16 +5,16 @@ pub fn render(window: &pancurses::Window, gb: &mut gap_buffer::GapBuffer) {
     let text = gb.to_string();
     let lines: Vec<&str> = text.split('\n').collect();
     window.clear();
-    let mut char_count:usize = 0;
-    let mut cursor_pos = (0,0);
+    let mut char_count: usize = 0;
+    let mut cursor_pos = (0, 0);
     for (i, line) in lines.iter().enumerate() {
         if gb.gap_start >= char_count && gb.gap_start <= char_count + line.len() {
-           cursor_pos.0 = i;
-cursor_pos.1 = gb.gap_start.wrapping_sub(char_count);
+            cursor_pos.0 = i;
+            cursor_pos.1 = gb.gap_start.wrapping_sub(char_count);
         }
         window.mv(i as i32, 0);
         window.addstr(line);
-        char_count += line.len()+1;
+        char_count += line.len() + 1;
     }
     let remaining_row_count = window.get_max_y() - lines.len() as i32;
     for i in 0..remaining_row_count {
