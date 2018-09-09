a6724d400621b8f7d1451aa6901224afd6948448
richard <richard>
2018-09-09 06:41:09 +0000

    shifting

--- src/gap_buffer.rs
+++ src/gap_buffer.rs
@@ -15,40 +15,72 @@ impl GapBuffer {
     }
 
     fn resize(&mut self, new_len: usize) {
-        println!("resize to {}",new_len);
         let mut l = self.data.len();
+        let end_count = l-self.gap_end;
+
         if new_len <= 2 {
             self.data.resize(new_len, ' ');
             self.gap_end = new_len;
         } else {
             if new_len > l {
+                //if we exceed size double size of vector
                 l *= 2;
+                self.data.resize(l, ' ');
+                //if we grow our size, move ending characters to new end
+                if end_count > 0 {
+                    for i in 0..end_count {
+                        self.data[l-end_count+i] = self.data[self.gap_end+i];
+                    }
+                }
             }
-            self.data.resize(l, ' ');
-            self.gap_end = l;
+            self.gap_end = l-end_count;
         }
     }
 
     pub fn insert_char(&mut self, c: char) {
-        let l = self.gap_start;
-        self.resize(l + 1);
+        let front_count = self.gap_start;
+        let end_count = self.data.len()-self.gap_end;
+        self.resize(front_count + 1 + end_count);
         self.data[self.gap_start] = c;
         self.gap_start += 1;
     }
 
     pub fn remove_char(&mut self) {
+        //if gap is at start do nothing
         if self.gap_start == 0 {
             return;
         }
-        
-        let mut l = self.gap_start-1;
-        if self.gap_start >= 0 {
-            self.resize(l);
-            self.gap_start = l;
+
+        let l = self.gap_start-1;
+        self.resize(l);
+        self.gap_start = l;
+    }
+
+    pub fn shift_gap_backward(&mut self) {
+        //if gap is already at the beginning do nothing
+        if self.gap_start == 0 {
+            return;
         }
+        self.gap_start -=1;
+        self.gap_end -=1;
+        self.data[self.gap_end] = self.data[self.gap_start];
+    }
+
+    pub fn shift_gap_forward(&mut self) {
+        //if we are at the end, do nothing
+        if self.gap_end == self.data.len() {
+            return;
+        }
+        self.gap_start +=1;
+        self.gap_end +=1;
+        self.data[self.gap_start] = self.data[self.gap_end-1];
     }
 
     pub fn to_string(&self) -> String {
-        return self.data[..self.gap_start].into_iter().collect();
+        //combine front and back characters into a string
+        let mut front:String = self.data[..self.gap_start].into_iter().collect();
+        let back:String =  self.data[self.gap_end..].into_iter().collect();
+        front.push_str(&back);
+        front
     }
 }
--- src/test.rs
+++ src/test.rs
@@ -117,3 +117,165 @@ fn basic_beyond_empty() {
     assert_eq!(gb.gap_end, 0);
     assert_eq!(gb.to_string(), "");
 }
+
+#[test]
+fn basic_move_left() {
+    let mut gb = gap_buffer::GapBuffer::new();
+    gb.insert_char('a');
+    gb.insert_char('b');
+    gb.insert_char('c');
+    gb.shift_gap_backward();
+    assert_eq!(gb.data[0], 'a');
+    assert_eq!(gb.data[1], 'b');
+    assert_eq!(gb.data[3], 'c');
+    assert_eq!(gb.data.len(), 4);
+    assert_eq!(gb.gap_start, 2);
+    assert_eq!(gb.gap_end, 3);
+    assert_eq!(gb.to_string(), "abc");
+}
+
+#[test]
+fn basic_move_backward_2() {
+    let mut gb = gap_buffer::GapBuffer::new();
+    gb.insert_char('a');
+    gb.insert_char('b');
+    gb.insert_char('c');
+    gb.shift_gap_backward();
+    gb.shift_gap_backward();
+    assert_eq!(gb.data[0], 'a');
+    assert_eq!(gb.data[2], 'b');
+    assert_eq!(gb.data[3], 'c');
+    assert_eq!(gb.data.len(), 4);
+    assert_eq!(gb.gap_start, 1);
+    assert_eq!(gb.gap_end, 2);
+    assert_eq!(gb.to_string(), "abc");
+}
+
+#[test]
+fn basic_move_backward_beyond() {
+    let mut gb = gap_buffer::GapBuffer::new();
+    gb.insert_char('a');
+    gb.insert_char('b');
+    gb.insert_char('c');
+    gb.shift_gap_backward();
+    gb.shift_gap_backward();
+    gb.shift_gap_backward();
+    gb.shift_gap_backward();
+    assert_eq!(gb.data[1], 'a');
+    assert_eq!(gb.data[2], 'b');
+    assert_eq!(gb.data[3], 'c');
+    assert_eq!(gb.data.len(), 4);
+    assert_eq!(gb.gap_start, 0);
+    assert_eq!(gb.gap_end, 1);
+    assert_eq!(gb.to_string(), "abc");
+}
+
+#[test]
+fn basic_move_forward() {
+    let mut gb = gap_buffer::GapBuffer::new();
+    gb.insert_char('a');
+    gb.insert_char('b');
+    gb.insert_char('c');
+    gb.shift_gap_backward();
+    gb.shift_gap_backward();
+    gb.shift_gap_forward();
+    assert_eq!(gb.data[0], 'a');
+    assert_eq!(gb.data[1], 'b');
+    assert_eq!(gb.data[3], 'c');
+    assert_eq!(gb.data.len(), 4);
+    assert_eq!(gb.gap_start, 2);
+    assert_eq!(gb.gap_end, 3);
+    assert_eq!(gb.to_string(), "abc");
+}
+
+#[test]
+fn basic_move_forward_beyond() {
+    let mut gb = gap_buffer::GapBuffer::new();
+    gb.insert_char('a');
+    gb.insert_char('b');
+    gb.insert_char('c');
+    gb.shift_gap_backward();
+    gb.shift_gap_forward();
+    gb.shift_gap_forward();
+    assert_eq!(gb.data[0], 'a');
+    assert_eq!(gb.data[1], 'b');
+    assert_eq!(gb.data[2], 'c');
+    assert_eq!(gb.data.len(), 4);
+    assert_eq!(gb.gap_start, 3);
+    assert_eq!(gb.gap_end, 4);
+    assert_eq!(gb.to_string(), "abc");
+}
+
+
+#[test]
+fn basic_middle_insert() {
+    let mut gb = gap_buffer::GapBuffer::new();
+    gb.insert_char('a');
+    gb.insert_char('b');
+    gb.insert_char('c');
+    gb.shift_gap_backward();
+    gb.insert_char('d');
+    assert_eq!(gb.data[0], 'a');
+    assert_eq!(gb.data[1], 'b');
+    assert_eq!(gb.data[2], 'd');
+    assert_eq!(gb.data[3], 'c');
+    assert_eq!(gb.data.len(), 4);
+    assert_eq!(gb.gap_start, 3);
+    assert_eq!(gb.gap_end, 3);
+    assert_eq!(gb.to_string(), "abdc");
+}
+
+#[test]
+fn basic_middle_insert_too_long() {
+    let mut gb = gap_buffer::GapBuffer::new();
+    gb.insert_char('a');
+    gb.insert_char('b');
+    gb.insert_char('c');
+    gb.shift_gap_backward();
+    gb.insert_char('d');
+    gb.insert_char('e');
+    assert_eq!(gb.data[0], 'a');
+    assert_eq!(gb.data[1], 'b');
+    assert_eq!(gb.data[2], 'd');
+    assert_eq!(gb.data[3], 'e');
+    assert_eq!(gb.data[7], 'c');
+    assert_eq!(gb.data.len(), 8);
+    assert_eq!(gb.gap_start, 4);
+    assert_eq!(gb.gap_end, 7);
+    assert_eq!(gb.to_string(), "abdec");
+}
+
+#[test]
+fn basic_middle_insert_too_long2() {
+    let mut gb = gap_buffer::GapBuffer::new();
+    gb.insert_char('a');
+    gb.insert_char('b');
+    gb.insert_char('c');
+    gb.shift_gap_backward();
+    gb.insert_char('d');
+    gb.insert_char('e');
+    gb.shift_gap_backward();
+    gb.insert_char('f');
+    assert_eq!(gb.data[0], 'a');
+    assert_eq!(gb.data[1], 'b');
+    assert_eq!(gb.data[2], 'd');
+    assert_eq!(gb.data[3], 'f');
+    assert_eq!(gb.data[6], 'e');
+    assert_eq!(gb.data[7], 'c');
+    assert_eq!(gb.data.len(), 8);
+    assert_eq!(gb.gap_start, 4);
+    assert_eq!(gb.gap_end, 6);
+    assert_eq!(gb.to_string(), "abdfec");
+}
+
+
+#[test]
+fn basic_repeat() {
+    let mut gb = gap_buffer::GapBuffer::new();
+    for _i in 0..100 {
+        gb.insert_char('a');
+    }
+    assert_eq!('a', gb.data[99]);
+    assert_eq!(' ', gb.data[100]);
+    assert_eq!(128, gb.data.len());
+}
